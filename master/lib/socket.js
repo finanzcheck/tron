64150211fb568314173ebf9aab8e27b2b4d116e5
Klaus Großmann <klaus.grossmann@finanzcheck.de>
2015-07-01 20:29:51 +0200

    refactored and cleaned socket abstraction

--- master/lib/socket.js
+++ master/lib/socket.js
@@ -1,20 +1,18 @@
 var serverService = require('./server');
 var socketEvents = require('./socketEvents');
 var debug = require('debug')('master:socket');
-//socket.io connection
-var io;
-
-function socketError(data, socket) {
-    io.emit(socketEvents.ERROR, data);
-}
+var SocketIO = require('socket.io');
 
 function Socket(server) {
-    // create connection;
-    if (server && null == io) {
-        io = require('socket.io')(server);
+    var self = this;
+
+    this.ioServer = new SocketIO(server);
+
+    function socketError(data) {
+        self.ioServer.emit(socketEvents.ERROR, data);
     }
 
-    io.on('connection', function (socket) {
+    this.ioServer.on('connection', function (socket) {
         socket.on(socketEvents.CLIENTS_GET, function (data) {
             debug([socketEvents.CLIENTS_GET, data]);
 
@@ -24,47 +22,43 @@ function Socket(server) {
         socket.on(socketEvents.CLIENT_SWITCH, function (data) {
             debug([socketEvents.CLIENT_SWITCH, data]);
 
-            io.emit(socketEvents.CLIENT_PENDING, data.id);
+            self.ioServer.emit(socketEvents.CLIENT_PENDING, data.id);
 
             serverService.switchTV(data.state, data.id, function (err) {
                 if (err) {
                     socketError({id: data.id, message: 'Error on switch TV!'});
                     return;
                 }
-                io.emit(socketEvents.CLIENT_UPDATE, data);
+                self.ioServer.emit(socketEvents.CLIENT_UPDATE, data);
             })
         });
 
         socket.on(socketEvents.CLIENT_CHANGEURL, function (data) {
             debug([socketEvents.CLIENT_CHANGEURL, data]);
-            io.emit(socketEvents.CLIENT_PENDING, data.id);
+            self.ioServer.emit(socketEvents.CLIENT_PENDING, data.id);
 
             serverService.changeUrl(data.url, data.id, function (err) {
                 if (err) {
                     socketError({id: data.id, message: 'Error on changeUrl!'});
                     return;
                 }
-                io.emit(socketEvents.CLIENT_UPDATE, data);
+                self.ioServer.emit(socketEvents.CLIENT_UPDATE, data);
             });
         });
 
         socket.on(socketEvents.CLIENT_CHANGETITLE, function (data) {
             debug([socketEvents.CLIENT_CHANGETITLE, data]);
-            io.emit(socketEvents.CLIENT_PENDING, data.id);
+            self.ioServer.emit(socketEvents.CLIENT_PENDING, data.id);
 
             serverService.changeTitle(data.title, data.id, function (err) {
                 if (err) {
                     socketError({id: data.id, message: 'Error on changeTitle!'});
                     return;
                 }
-                io.emit(socketEvents.CLIENT_UPDATE, data);
+                self.ioServer.emit(socketEvents.CLIENT_UPDATE, data);
             });
         });
-
-
     });
 }
 
 module.exports = Socket;
-
-
