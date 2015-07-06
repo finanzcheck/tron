60c314a7800cf7cf5777c5aa23fc0251bd185b9d
Stefan Schult <stefan.schult@finanzcheck.de>
2015-07-06 07:13:26 +0200

    fixed double client bug

--- master/lib/clientPool.js
+++ master/lib/clientPool.js
@@ -3,22 +3,40 @@ var Client = require('../../lib/client');
 
 require('array.prototype.find');
 
+/**
+ *
+ * @constructor
+ */
 function ClientPool() {
 
 }
 
 util.inherits(ClientPool, Array);
 
+/**
+ *
+ * @param id
+ * @returns {T}
+ */
 ClientPool.prototype.getById = function (id) {
     return this.find(function (client) {
         return client.id == id;
     });
 };
 
+/**
+ *
+ * @returns {Array.<T>}
+ */
 ClientPool.prototype.getArrayCopy = function () {
     return this.slice();
 };
 
+/**
+ *
+ * @param clients
+ * @returns {ClientPool}
+ */
 ClientPool.fromArray = function (clients) {
     var clientPool = new ClientPool();
     clients.forEach(function (client) {
